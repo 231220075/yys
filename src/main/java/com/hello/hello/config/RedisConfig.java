db22587936a0e9e144d10c2c622ea4f4809cceba
231220075 <231220075@smail.nju.edu.cn>
2025-07-18 19:17:45 +0800

    v2.5

--- src/main/java/com/hello/hello/config/RedisConfig.java
+++ src/main/java/com/hello/hello/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.hello.hello.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,21 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class RedisConfig {
     
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+    
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+    
     @Bean
     public JedisPool jedisPool() {
-        return new JedisPool(new JedisPoolConfig(), "localhost", 6379);
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(8);
+        config.setMaxIdle(8);
+        config.setMinIdle(0);
+        config.setTestOnBorrow(true);
+        config.setTestOnReturn(true);
+        config.setTestWhileIdle(true);
+        return new JedisPool(config, redisHost, redisPort);
     }
 }
